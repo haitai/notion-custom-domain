1e96d0b327dcf6b42663535e10e1b90332c9221d
Haitai Hu <hityhoo@gmail.com>
2025-08-11 21:53:37 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -123,7 +123,7 @@ const customScript = () => {
 };
 
 const customStyle = `
-  .notion-topbar > div > div:nth-last-child(1), .notion-topbar > div > div:nth-last-child(2),.notion-selectable { 
+  .notion-topbar > div > div:nth-last-child(1), .notion-topbar > div > div:nth-last-child(2),div:[contenteditable] { 
     display:none !important; 
   }
 `;
