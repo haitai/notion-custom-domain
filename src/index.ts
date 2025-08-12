9efe4871c2d66b70d784504921542a38adae2a9e
Haitai Hu <hityhoo@gmail.com>
2025-08-12 13:36:28 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -127,7 +127,7 @@ const customStyle = `
  { 
     display:none !important; 
   }
-  div[role="menuitem"]:nth-of-type(1){}
+  div[role="menuitem"]:nth-of-type(1){display:inline-block !important;position:fixed !important;right:0  !important;top:0 !important }
 `;
 
 function getCustomScript() {
